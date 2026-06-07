6d7a10459ce2570effb0f3a44586c1a2151c5119
Gison <gison@192.168.2.3>
2026-06-07 18:12:52 +0800

    add loop, delete function

--- Guitar-memory.py
+++ Guitar-memory.py
@@ -14,51 +14,73 @@ def Show_records():
             print(f"{index}: {row}")
 
 
-What_need = input("Do you wanna add more data? (y/n/e): ")
-if What_need.lower() == "y":
-    Your_practice_time = input("Enter your time: ")
-    Your_practice_song = input("Enter your song: ")
-    Your_discover = input("Enter your discover: ")
-    now = datetime.now().strftime("%Y-%m-%d")
-    What_time = input("Please enter a valid time: ")
-    if What_time == "":
-        what_time = now
-    else:
-        what_time = What_time
+while True:
+    What_need = input("Do you wanna add more data? (y/n/e/d): ")
+    if What_need.lower() == 'exit':
+        print("exit")
+        break
 
-    with open(Guitar_file, 'a', encoding='utf-8', newline='') as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_NONE, escapechar="\\")
-        if new_guitar:
-            writer.writerow(header)
-            new_guitar = False
-        writer.writerow([what_time, Your_practice_time, Your_practice_song,
-                         Your_discover])
+    elif What_need.lower() == "y":
+        Your_practice_time = input("Enter your time: ")
+        Your_practice_song = input("Enter your song: ")
+        Your_discover = input("Enter your discover: ")
+        now = datetime.now().strftime("%Y-%m-%d")
+        What_time = input("Please enter a valid time: ")
+        if What_time == "":
+            what_time = now
+        else:
+            what_time = What_time
 
-    print("Data saved to Guitar.csv")
+            with open(Guitar_file, 'a', encoding='utf-8', newline='') as file:
+                writer = csv.writer(
+                    file, quoting=csv.QUOTE_MINIMAL, escapechar="\\")
+                if new_guitar:
+                    writer.writerow(header)
+                new_guitar = False
+                writer.writerow([what_time, Your_practice_time, Your_practice_song,
+                                Your_discover])
+                print("Data saved to Guitar.csv")
 
-elif What_need.lower() == 'n':
-    Show_records()
+    elif What_need.lower() == 'n':
+        Show_records()
 
-elif What_need.lower() == 'e':
-    Show_records()
+    elif What_need.lower() == 'e':
+        Show_records()
+        with open("Guitar.csv", 'r', newline='', encoding='utf-8') as file:
+            csv_reader = csv.reader(file)
+            all_rows = list(csv_reader)
+        Change = int(input(
+            "What line do you want to change? (Enter the line number): "))
+        try:
+            if Change < 0 or Change >= len(all_rows):
+                print("Please enter a valid munber")
+        except ValueError:
+            print("Please enter a valid number")
+            print(all_rows[Change])
+        for index, header_name in enumerate(header, start=0):
+            print(f"{index}: {header_name}")
+        Change_what = int(input("What column do you wanna change?: "))
+        new_value = input("Enter the new value: ")
+        index = Change_what
+        all_rows[Change][index] = new_value
+        with open("Guitar.csv", 'w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file, quoting=csv.QUOTE_NONE, escapechar="\\")
+            writer.writerows(all_rows)
+        print("Done")
+    elif What_need.lower() == 'd':
+        Show_records()
     with open("Guitar.csv", 'r', newline='', encoding='utf-8') as file:
         csv_reader = csv.reader(file)
         all_rows = list(csv_reader)
-    Change = int(input(
-        "What line do you want to change? (Enter the line number): "))
-    try:
-        if Change < 0 or Change >= len(all_rows):
-            print("Please enter a valid munber")
-    except ValueError:
-        print("Please enter a valid number")
-    print(all_rows[Change])
-    for index, header_name in enumerate(header, start=0):
-        print(f"{index}: {header_name}")
-    Change_what = int(input("What column do you wanna change?: "))
-    new_value = input("Enter the new value: ")
-    index = Change_what
-    all_rows[Change][index] = new_value
-    with open("Guitar.csv", 'w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_NONE, escapechar="\\")
-        writer.writerows(all_rows)
-    print("Done")
+        Delete = int(input("What line do you wanna delete? : "))
+        try:
+            if Delete < 0 or Delete >= len(all_rows):
+                print("Please enter a valid number")
+        except:
+            print("Please enter a valid number")
+        del all_rows[Delete]
+        with open("Guitar.csv", 'w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(
+                file, quoting=csv.QUOTE_NONE, escapechar='\\')
+            writer.writerows(all_rows)
+        print("Done")
